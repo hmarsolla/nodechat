f2114fa59b495a71a78499463a1e9d24a7899b14
hmarsolla <hmarsolla@hotmail.com>
2018-01-23 22:58:32 -0200

    Emit newMessage on CreateMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on("connect", function () {
     console.log("Connected to server.");
-
-    socket.emit("createMessage", {
-        from: "Jenny",
-        text: "Hey. What up?!",
-    });
-
 });
 
 socket.on("disconnect", function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(path.join(__dirname, "../public")));
 io.on("connection", (socket) => {
     console.log("New user connected");
    
-    socket.emit("newMessage", {
-        from: "Mike",
-        text: "Hey. Buddy!",
-        createdAt: new Date().toUTCString()
-    });
-
-    socket.on("createMessage", (newEmail) => {
-        console.log("createMessage", newEmail);
+    socket.on("createMessage", (message) => {
+        console.log("createMessage", message);
+        io.emit("newMessage", {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on("disconnect", () => {
