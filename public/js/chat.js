2def12293d8b0fc92938fd77322bc7578ac75a04
hmarsolla <hmarsolla@hotmail.com>
2018-01-27 15:56:11 -0200

    Send messages to only users on the same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -39,7 +39,7 @@ socket.on("updateUserList", function (users) {
     users.forEach(function (user) {
         ol.append(jQuery("<li></li>").text(user));
     });
-    
+
     jQuery("#users").html(ol);
 });
 
@@ -75,7 +75,6 @@ jQuery("#message-form").on("submit", function (e) {
     var messageBox = jQuery("[name=message]");
 
     socket.emit("createMessage", {
-        from: "User",
         text: messageBox.val()
     }, function () {
         messageBox.val("");
