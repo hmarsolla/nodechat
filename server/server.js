4d5bcaea6c90dc562766f9cb320ebfa776b8b23a
hmarsolla <hmarsolla@hotmail.com>
2018-01-27 15:46:24 -0200

    Added User class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -4,29 +4,34 @@ const express = require("express");
 const socketIO = require("socket.io");
 const {generateMessage, generateLocationMessage} = require("./utils/message");
 const {isRealString} = require("./utils/validation");
+const {Users} = require("./utils/users");
 
 var app = express();
 const port = process.env.PORT || 3000;
 var server = http.createServer(app);
 
 var io = socketIO(server);
-
+var users = new Users();
 app.use(express.static(path.join(__dirname, "../public")));
+
+
 
 io.on("connection", (socket) => {
     console.log("New user connected");
    
     socket.on("join", (params, callback) => {
         if(!isRealString(params.name) || !isRealString(params.room)){
-            callback("Name and Room Name are required!");
+            return callback("Name and Room Name are required!");
         }
 
         socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
+        
+        io.to(params.room).emit("updateUserList", users.getUserList(params.room));
 
-        
-        
         socket.emit("newMessage", generateMessage("Admin", "Welcome to the chat app"));
-        socket.broadcast.to(params.room).emit("newMessage", generateMessage("Admin", `${params.name} has joined`));
+        socket.broadcast.to(params.room).emit("newMessage", generateMessage("Admin", `${params.name} has joined.`));
         callback();
     });
 
@@ -41,7 +46,11 @@ io.on("connection", (socket) => {
     });
 
     socket.on("disconnect", () => {
-        console.log("User disconnected from server");
+        var user = users.removeUser(socket.id);
+        if(user){
+            io.to(user.room).emit("updateUserList", users.getUserList(user.room));
+            io.to(user.room).emit("newMessage", generateMessage("Admin", `${user.name} has left.`));
+        }
     });
 });
 
